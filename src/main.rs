7e0f2f5efd1bfcffa393499683b485324e6e7792
Emil <emilbabazade29@gmail.com>
2024-05-08 20:35:47 +0300

    get cmd args and double print them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("double_echo_rust")
+        .about("double prints the inputs")
+        .author("Emil Babazade")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("text to double print")
+                .required(true)
+                .min_values(1),
+        )
+        .get_matches();
+
+    let texts = matches.values_of_lossy("text").unwrap();
+    let text = texts.join(" ");
+    print!("{}\n{}", text, text);
 }
