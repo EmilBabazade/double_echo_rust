e97624b3c8ceec4bb290a77ee2a3036952a6dddc
Emil <emilbabazade29@gmail.com>
2024-05-08 20:46:47 +0300

    line seperator

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,21 @@ fn main() {
                 .required(true)
                 .min_values(1),
         )
+        .arg(
+            Arg::with_name("seperator")
+                .short("s")
+                .required(false)
+                .help("seperator between double printed lines")
+                .number_of_values(1),
+        )
         .get_matches();
+
+    let line_seperator = match matches.value_of("seperator") {
+        Some(str) => "\n".to_owned() + str + "\n",
+        _ => "\n".to_owned(),
+    };
 
     let texts = matches.values_of_lossy("text").unwrap();
     let text = texts.join(" ");
-    print!("{}\n{}", text, text);
+    print!("{}{}{}", text, line_seperator, text);
 }
